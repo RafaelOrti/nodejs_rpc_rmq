2c69a9a3f96e1f758fe4010c268013df41b9fdb1
Rafa <raorcar3@gmail.com>
2023-12-13 21:07:08 +0100

    Define /wishlist endpoint RPCRequest and fake requestPayload to obtain fake response from RPCObserver

--- src/customer.js
+++ src/customer.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { RPCObserver } = require("./rpc");
+const { RPCObserver, RPCRequest } = require("./rpc");
 const PORT = 9000;
 
 const app = express();
@@ -13,11 +13,22 @@ const fakeCustomerResponse = {
 
 RPCObserver("CUSTOMER_RPC", fakeCustomerResponse)
 
-app.get("/wishlist", (req,res) => {
-    return res.json("Customer Service")
-})
+app.get("/wishlist", async (req, res) => {
+    const requestPayload = {
+        productId: "123",
+        customerId: "yt686tu8763tyyr98734",
+    };
+    try {
+        const responseData = await RPCRequest("PRODUCT_RPC", requestPayload);
+        console.log(responseData);
+        return res.status(200).json(responseData);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+});
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     return res.json("Customer Service")
 })
 
