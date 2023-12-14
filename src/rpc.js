ac9c457ec55ee0655f527e8be23cf7b72df625f8
Rafa <raorcar3@gmail.com>
2023-12-14 17:48:57 +0100

    Add a timeout to limitate the promise waiting time

--- src/rpc.js
+++ src/rpc.js
@@ -15,10 +15,10 @@ const expensiveDBOperation = (payload, fakeResponse) => {
     console.log(payload);
     console.log(fakeResponse);
 
-    return new Promise ((res, rej) => {
+    return new Promise((res, rej) => {
         setTimeout(() => {
             res(fakeResponse);
-        }, 3000)
+        }, 9000)
     })
 }
 
@@ -29,17 +29,17 @@ const RPCObserver = async (RPC_QUEUE_NAME, fakeResponse) => {
     await channel.assertQueue(RPC_QUEUE_NAME, {
         durable: false,
     });
-    
+
     channel.prefetch(1);
-    
+
     channel.consume(
         RPC_QUEUE_NAME,
         async (msg) => {
-            
+
             if (msg.content) {
                 // DB operation
-                    const payload = JSON.parse(msg.content.toString());
-                    const response = await expensiveDBOperation(payload, fakeResponse); //call fake DB response
+                const payload = JSON.parse(msg.content.toString());
+                const response = await expensiveDBOperation(payload, fakeResponse); //call fake DB response
                 channel.sendToQueue(
                     msg.properties.replyTo,
                     Buffer.from(JSON.stringify(response)),
@@ -50,8 +50,8 @@ const RPCObserver = async (RPC_QUEUE_NAME, fakeResponse) => {
                 channel.ack(msg);
             }
         }, {
-            noAck: false,
-        }
+        noAck: false,
+    }
     )
 
 };
@@ -61,15 +61,25 @@ const requestData = async (RPC_QUEUE_NAME, requestPayload, uuid) => {
 
     const q = await channel.assertQueue("", { exclusive: true });
 
-    channel.sendToQueue( RPC_QUEUE_NAME, Buffer.from(JSON.stringify(requestPayload)), {
-            replyTo: q.queue,
-            correlationId: uuid,
+    channel.sendToQueue(
+        RPC_QUEUE_NAME, 
+        Buffer.from(JSON.stringify(requestPayload)), 
+        {
+        replyTo: q.queue,
+        correlationId: uuid,
     });
 
-    return new Promise((resolve,reject) => {
+    
+    return new Promise((resolve, reject) => {
+        // timeout n seconds
+        const timeout = setTimeout(() => {
+            channel.close();
+            resolve("API could not fullfil the request!");
+        }, 8000);
         channel.consume(q.queue, (msg) => {
             if (msg.properties.correlationId == uuid) {
                 resolve(JSON.parse(msg.content.toString()));
+                clearTimeout(timeout);
             } else {
                 reject("Data not found");
             }
@@ -81,7 +91,7 @@ const requestData = async (RPC_QUEUE_NAME, requestPayload, uuid) => {
 
 
 const RPCRequest = async (RPC_QUEUE_NAME, requestPayload) => {
-    
+
     const uuid = uuid4();
     return await requestData(RPC_QUEUE_NAME, requestPayload, uuid)
 };
